7612dc19af13857755a0c70aaea4eb7792cbad1d
Giancarlos Salas <giansalex@gmail.com>
2021-08-23 18:23:09 -0500

    Add initial pages

---
+++ src/App/pages/collectibles/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import {
+  Box,
+} from "@chakra-ui/react"
+
+export const Collectibles = () => (
+    <Box textAlign="center" fontSize="xl">
+        Collectibles
+    </Box>
+)
---
+++ src/App/pages/create/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import {
+  Box,
+} from "@chakra-ui/react"
+
+export const Create = () => (
+    <Box textAlign="center" fontSize="xl">
+        Create
+    </Box>
+)
