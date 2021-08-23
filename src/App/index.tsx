60bbf28f1c545f4f50b7f5a0fc3b70674ce8e6a9
Giancarlos Salas <giansalex@gmail.com>
2021-08-23 18:19:23 -0500

    Move app to dir

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -10,7 +10,6 @@ import {
   theme,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -18,7 +17,6 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
           <Text>
             Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
           </Text>
