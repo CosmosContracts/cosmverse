6f1971f2cf1342a7c6d6eaa677d360c0353efeaf
Giancarlos Salas <giansalex@gmail.com>
2021-08-23 18:28:21 -0500

    Add landing page

---
+++ src/App/pages/landing/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import {
+  Box,
+} from "@chakra-ui/react"
+
+export const Landing = () => (
+    <Box textAlign="center" fontSize="xl">
+        Create
+    </Box>
+)
