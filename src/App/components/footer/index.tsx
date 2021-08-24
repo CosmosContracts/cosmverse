e4172b026fceb482782eec9ea9cf404608d63c0c
Giancarlos Salas <giansalex@gmail.com>
2021-08-23 22:23:47 -0500

    Remove unused icons

--- src/App/components/footer/index.tsx
+++ src/App/components/footer/index.tsx
@@ -3,13 +3,12 @@ import {
   Box,
   chakra,
   Container,
-  Link,
   Stack,
   Text,
   useColorModeValue,
   VisuallyHidden,
 } from '@chakra-ui/react';
-import { FaDiscord, FaInstagram, FaTelegram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { FaDiscord, FaTelegram, FaTwitter } from 'react-icons/fa';
 import { ReactNode } from 'react';
   
   
