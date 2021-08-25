11b7752e176e9eb08780ae89632fd9f1e4e714f9
Giancarlos Salas <giansalex@gmail.com>
2021-08-25 18:28:52 -0500

    Add temp random nft

--- src/App/components/nft-section/index.tsx
+++ src/App/components/nft-section/index.tsx
@@ -16,12 +16,7 @@ interface NftSectionProps {
 }
 
 export function NftSection({ title }: NftSectionProps): JSX.Element {
-  const nft: NftInfo = {
-  image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=320&q=80",
-    title: "NIKE Air",
-    user: "Mistic",
-    price: "5 JUNO",
-  };
+
   return (
     <Box>
       <Flex
@@ -45,12 +40,33 @@ export function NftSection({ title }: NftSectionProps): JSX.Element {
         </Button>
       </Flex>
       <Grid templateColumns="repeat(5, 1fr)" gap={6}>
-        <NftCard nft={nft} />
-        <NftCard nft={nft} />
-        <NftCard nft={nft} />
-        <NftCard nft={nft} />
-        <NftCard nft={nft} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
       </Grid>
     </Box>
   );
+}
+
+function randomNft(): NftInfo {
+  const price = (Math.random() * 10).toFixed(1);
+  const rand = Math.floor((Math.random() * 100) % 2);
+
+  if (rand === 1) {
+    return {
+      image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=320&q=80",
+      title: "NIKE Air",
+      user: "Mistic",
+      price: price + " JUNO",
+    };
+  }
+
+  return {
+    image: "https://images.unsplash.com/photo-1585336261022-680e295ce3fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80",
+    title: "Pen cosm",
+    user: "RealMint",
+    price: price + " JUNO",
+  };
 }
