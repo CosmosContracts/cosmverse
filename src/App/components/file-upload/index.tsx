3ff22a720fb69c70e5612066806901e6418948f4
Giancarlos Salas <giansalex@gmail.com>
2021-08-28 17:39:42 -0500

    Add base form to create nft

---
+++ src/App/components/file-upload/index.tsx
@@ -0,0 +1,92 @@
+import * as React from "react";
+import {
+  chakra,
+  Flex,
+  Icon,
+  Stack,
+  Text,
+  useColorModeValue,
+  VisuallyHidden,
+} from "@chakra-ui/react";
+import { useRef } from "react";
+
+type FileUploadProps = {
+  accept?: string
+  multiple?: boolean
+}
+
+export function FileUpload({ accept, multiple }: FileUploadProps): JSX.Element {
+  // const inputRef = useRef();
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+
+  return (
+    <Flex
+      mt={1}
+      justify="center"
+      px={6}
+      pt={5}
+      pb={6}
+      borderWidth={2}
+      borderColor={useColorModeValue("gray.300", "gray.500")}
+      borderStyle="dashed"
+      rounded="md"
+      onClick={() => inputRef.current?.click()}
+    >
+      <Stack spacing={1} textAlign="center">
+        <Icon
+          mx="auto"
+          boxSize={12}
+          color={useColorModeValue("gray.400", "gray.500")}
+          stroke="currentColor"
+          fill="none"
+          viewBox="0 0 48 48"
+          aria-hidden="true"
+        >
+          <path
+            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </Icon>
+        <Flex
+          fontSize="sm"
+          color={useColorModeValue("gray.600", "gray.400")}
+          alignItems="baseline"
+        >
+          <chakra.label
+            htmlFor="file-upload"
+            cursor="pointer"
+            rounded="md"
+            fontSize="md"
+            color={useColorModeValue("brand.600", "brand.200")}
+            pos="relative"
+            _hover={{
+              color: useColorModeValue("brand.400", "brand.300"),
+            }}
+          >
+            <span>Upload a file</span>
+            <VisuallyHidden>
+              <input
+                accept={accept}
+                multiple={multiple}
+                id="file-upload"
+                name="file-upload"
+                type="file"
+                ref={e => inputRef.current = e}
+              />
+            </VisuallyHidden>
+          </chakra.label>
+          <Text pl={1}>or drag and drop</Text>
+        </Flex>
+        <Text
+          fontSize="xs"
+          color={useColorModeValue("gray.500", "gray.50")}
+        >
+          PNG, JPG, GIF up to 10MB
+        </Text>
+      </Stack>
+    </Flex>
+  );
+}
--- src/App/pages/create/index.tsx
+++ src/App/pages/create/index.tsx
@@ -1,10 +1,77 @@
 import * as React from "react"
 import {
   Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Textarea,
 } from "@chakra-ui/react"
+import { FileUpload } from "../../components/file-upload"
 
 export const Create = () => (
-    <Box textAlign="center" fontSize="xl">
-        Create
+  <Flex
+    py={{ base: 5 }}
+    px={{ base: 4 }}
+    borderBottom={1}
+    justifyContent="center"
+    alignItems="center"
+    direction="row">
+    <Box maxW="500px" w="100%">
+      <Box>
+        <Box mt={6} mb={10}>
+          <Heading as="h3" fontSize="3xl">Create a single NFT</Heading>
+        </Box>
+        <Box as={'form'} id="nft-form">
+        <Box>
+            <FormControl id="name" isRequired>
+              <FormLabel
+                fontSize="sm"
+                fontFamily="mono"
+                fontWeight="semibold"
+              >Image</FormLabel>
+              <FileUpload accept="image/*"/>
+            </FormControl>
+          </Box>
+          <Box mt={4}>
+            <FormControl id="name" isRequired>
+              <FormLabel
+                fontSize="sm"
+                fontFamily="mono"
+                fontWeight="semibold"
+              >Name</FormLabel>
+              <Input name="name" />
+            </FormControl>
+          </Box>
+          <Box mt={4}>
+            <FormControl id="description">
+              <FormLabel
+                fontSize="sm"
+                fontFamily="mono"
+                fontWeight="semibold"
+              >Description</FormLabel>
+              <Textarea name="description" placeholder="NFT description" />
+            </FormControl>
+          </Box>
+          <Box mt={6}>
+            <Button
+              height="var(--chakra-sizes-10)"
+              fontSize={'md'}
+              fontWeight="semibold"
+              borderRadius={'50px'}
+              color={'white'}
+              bg="pink.500"
+              _hover={{
+                bg: "pink.700",
+              }}>
+              Create
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+
     </Box>
+  </Flex>
 )
