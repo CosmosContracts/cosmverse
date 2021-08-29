ae66bab12d6d50ee2982e3977a053d21bbef090a
Giancarlos Salas <giansalex@gmail.com>
2021-08-29 15:33:40 -0500

    Add testnet configs

---
+++ src/config.ts
@@ -0,0 +1,34 @@
+import { AppConfig, getAppConfig, NetworkConfigs } from "./App/services/config/network";
+
+const local: AppConfig = {
+  chainId: "testing",
+  chainName: "Testing",
+  addressPrefix: "juno",
+  rpcUrl: "http://localhost:26657",
+  httpUrl: "http://localhost:1317",
+  token: {
+    coinDenom: "STAKE",
+    coinDecimals: 6,
+    coinMinimalDenom: "ustake"
+  },
+  gasPrice: 0.025,
+  codeId: 4,
+};
+
+const testnet: AppConfig = {
+  chainId: "lucina",
+  chainName: "Juno Tesnet",
+  addressPrefix: "juno",
+  rpcUrl: "https://rpc.juno.omniflix.co",
+  httpUrl: "https://api.juno.omniflix.co",
+  token: {
+    coinDenom: "JUNO",
+    coinDecimals: 6,
+    coinMinimalDenom: "ujuno"
+  },
+  gasPrice: 0.025,
+  codeId: 4,
+};
+
+const configs: NetworkConfigs = { local, testnet };
+export const config = getAppConfig(configs);
