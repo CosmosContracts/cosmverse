2bad5c231bb10fc20e7461b2928d2e173aaa9f2c
Giancarlos Salas <giansalex@gmail.com>
2021-09-02 15:33:24 -0500

    Add gallery make nfts

--- src/App/pages/gallery/index.tsx
+++ src/App/pages/gallery/index.tsx
@@ -1,10 +1,28 @@
 import * as React from "react"
 import {
-  Box,
+  Box, SimpleGrid,
 } from "@chakra-ui/react"
+import { NftCard } from "../../components/nft-card";
+import { randomNft } from "../../components/nft-card/rand";
 
-export const Gallery = () => (
-    <Box textAlign="center" fontSize="xl">
-        Gallery
+export const Gallery = () => {
+  return (
+    <Box m={5}>
+      <SimpleGrid columns={5} spacing={10}>
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+        <NftCard nft={randomNft()} />
+      </SimpleGrid>
     </Box>
-)
+  );
+};
