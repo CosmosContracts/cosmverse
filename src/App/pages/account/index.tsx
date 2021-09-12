7caa89e7e9ad73ccf990a018d79d9f7d168a57e9
Giancarlos Salas <giansalex@gmail.com>
2021-09-11 21:19:32 -0500

    Use external user-logo

--- src/App/pages/account/index.tsx
+++ src/App/pages/account/index.tsx
@@ -20,7 +20,7 @@ import { CW721, NftInfoResponse } from "../../services/client/cw721";
 import { config } from "../../../config";
 import { publicIpfsUrl } from "../../services/ipfs/client";
 import { NftCard } from "../../components/nft-card";
-
+import userLogo from "../../assets/user-default.svg";
 
 export const Account = () => {
   const { address } = useSdk();
@@ -66,7 +66,7 @@ export const Account = () => {
 						<Image
 							borderRadius="full"
 							boxSize="120px"
-							src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScxMjAnIGhlaWdodD0nMTIwJyBzdHlsZT0nYmFja2dyb3VuZC1jb2xvcjpyZ2JhKDI0NiwyNDYsMjQ2LDEpOyc+PGcgc3R5bGU9J2ZpbGw6cmdiYSgyMDQsMzgsMjE3LDEpOyBzdHJva2U6cmdiYSgyMDQsMzgsMjE3LDEpOyBzdHJva2Utd2lkdGg6MC42Oyc+PHJlY3QgIHg9JzU1JyB5PSczNScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9JzU1JyB5PSc0NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9JzU1JyB5PSc1NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9JzU1JyB5PSc2NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9JzQ1JyB5PSc1NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9JzY1JyB5PSc1NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9JzQ1JyB5PSc3NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9JzY1JyB5PSc3NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9JzM1JyB5PSc0NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9Jzc1JyB5PSc0NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9JzM1JyB5PSc1NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PHJlY3QgIHg9Jzc1JyB5PSc1NScgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJy8+PC9nPjwvc3ZnPg==" />
+							src={userLogo} />
 					</Box>
 					<Box bg="blackAlpha.300" borderRadius="xl" py={1} px={3}>
 						<Text color={"gray.500"} fontFamily="mono" fontSize="sm">
