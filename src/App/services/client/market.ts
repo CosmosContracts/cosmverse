72f84a2e53fca9c2da5cbd82b6a660f8beb2341a
Giancarlos Salas <giansalex@gmail.com>
2021-09-13 16:58:37 -0500

    Add nft market client

---
+++ src/App/services/client/market.ts
@@ -0,0 +1,89 @@
+import { CosmWasmClient, SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { Coin } from "@cosmjs/stargate";
+
+export interface OfferResponse {
+  contract: string
+  id: string
+  list_price: Coin
+  seller: string,
+  token_id: string
+}
+
+export interface OffersResponse {
+  offers: OfferResponse[]
+}
+
+export interface MarketInstance {
+  readonly contractAddress: string;
+
+  numOffers: () => Promise<number>;
+  offer: (contract: string, tokenId: string) => Promise<OfferResponse>;
+  offersBySeller: (seller: string, startAfter?: string, limit?: number) => Promise<OffersResponse>;
+  allOffers: (startAfter?: string, limit?: number) => Promise<OffersResponse>;
+}
+
+export interface MarketTxInstance {
+  readonly contractAddress: string;
+
+  // actions
+  buy: (sender: string, offerId: string) => Promise<string>;
+  withdraw: (sender: string, offerId: string) => Promise<string>;
+}
+
+export interface MarketContract {
+  use: (client: CosmWasmClient) => MarketInstance;
+  useTx: (client: SigningCosmWasmClient) => MarketTxInstance;
+}
+
+export const Market = (contractAddress: string): MarketContract => {
+  const use = (client: CosmWasmClient): MarketInstance => {
+
+    const numOffers = async (): Promise<number> => {
+      const result = await client.queryContractSmart(contractAddress, { get_count: {} });
+      return result.count;
+    };
+
+    const offer = async (contract: string, tokenId: string): Promise<OfferResponse> => {
+      const result = await client.queryContractSmart(contractAddress, { get_offer: { contract, token_id: tokenId } });
+      return result;
+    };
+
+    const offersBySeller = async (seller: string, startAfter?: string, limit?: number): Promise<OffersResponse> => {
+      const result = await client.queryContractSmart(contractAddress, { get_offers: { seller, start_after: startAfter, limit: limit } });
+      return result;
+    };
+
+    const allOffers = async (startAfter?: string, limit?: number): Promise<OffersResponse> => {
+      const result = await client.queryContractSmart(contractAddress, { all_offers: { start_after: startAfter, limit: limit } });
+      return result;
+    };
+
+    return {
+      contractAddress,
+      numOffers,
+      offer,
+      offersBySeller,
+      allOffers,
+    }
+  };
+
+  const useTx = (client: SigningCosmWasmClient): MarketTxInstance => {
+    const buy = async (sender: string, offerId: string): Promise<string> => {
+      const result = await client.execute(sender, contractAddress, { buy: { offering_id: offerId } });
+      return result.transactionHash;
+    };
+
+    const withdraw = async (sender: string, offerId: string): Promise<string> => {
+      const result = await client.execute(sender, contractAddress, { withdraw_nft: { offering_id: offerId } });
+      return result.transactionHash;
+    };
+
+    return {
+      contractAddress,
+      buy,
+      withdraw,
+    };
+  };
+
+  return { use, useTx };
+}
