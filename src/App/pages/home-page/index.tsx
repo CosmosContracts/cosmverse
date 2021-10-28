0fec6f18eae8ebe90d62fe25d0b2c2855e01bae5
axppp <pauloponcianoo@gmail.com>
2021-10-28 23:28:22 +0100

    Adding layers for the new Landing page and Scss

--- src/App/pages/home-page/index.tsx
+++ src/App/pages/home-page/index.tsx
@@ -8,75 +8,60 @@ Button,
 Flex,
 Grid,
 GridItem,
-Stack,
-Text
+Image,
+Stack
 } from "@chakra-ui/react"
 import { FaDiscord, FaTelegram, FaTwitter } from 'react-icons/fa';
 
 import { SocialButton } from "../../components";
+import cosmverseText from "../../assets/cosmoverse-fit.svg";
 
 export const HomePage = () => {
   return (
+    <React.Fragment>
+        <Box w="100vw" h="100vh" className='home'>
+          <Image maxH='36vh' src={cosmverseText} className='text'>
+          </Image>
+        </Box>
 
-    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center" className='homepage'>
+      <Box  className='planet'>
+      </Box>
 
-      <Grid h="100%" w="100%" templateColumns="repeat(3, 1fr)" templateRows="repeat(3,1fr)">
-        <GridItem colSpan={3} rowSpan={1} bg="papayawhip" colStart={1} colEnd={4}>
+      <Box w="100vw" h="100vh" className='stars'>
+      </Box>
 
-          <Flex h="100%" w="100%"alignItems="center" justifyContent="center">
-              <Text fontFamily={'mono'}  color='white'>
-                    CosmVerse
-                </Text>
-            </Flex>
-
-        </GridItem>
-
-
-        <GridItem colSpan={1} colStart={2} colEnd={3} rowSpan={1} >
-        <Box w='3xl' h="3xl" className='planet'>
-          </Box>
-        </GridItem>
-        <GridItem colSpan={1}  colStart={3} colEnd={4} rowSpan={1} >
-          <Flex h="100%" w="100%"alignItems="center" justifyContent="center" flexDirection='column'>
-
+      <Box className='content'>
           <Button
-            fontSize={'sm'}
-            fontWeight={500}
-            variant={'outline'}
-            borderRadius="50px"
-            height="var(--chakra-sizes-8)"
-            marginTop={"4px"}
-            backgroundColor='#ffffffff'
-            href='#'
-          >
-            Enter the CosmVerse
-          </Button>
+              fontSize={'sm'}
+              size='lg'
+              fontWeight={500}
+              variant={'outline'}
+              borderRadius='50px'
+              height='var(--chakra-sizes-8)'
+              marginTop={'4px'}
+              backgroundColor='#1e7880'
+              href='#'
+            >
+              Enter the CosmVerse
+            </Button>
 
-            <Stack direction={'row'} backgroundColor='#ffffffff'>
-              <SocialButton label={'Twitter'} href={'https://twitter.com/Cosmverse'} width='19' height='19'>
-                <FaTwitter />
-              </SocialButton>
-              <SocialButton label={'Telegram'} href={'https://t.me/Cosmverse_community'} width='19' height='19'>
-                <FaTelegram />
-              </SocialButton>
-              <SocialButton label={'Discord'} href={'https://discord.gg/6tbnz7ss'} width='19' height='19'>
-                <FaDiscord />
-              </SocialButton>
+              <Stack direction={'row'} backgroundColor='#1e7880'>
+                <SocialButton label={'Twitter'} href={'https://twitter.com/Cosmverse'} width='25' height='25'>
+                  <FaTwitter />
+                </SocialButton>
+                <SocialButton label={'Telegram'} href={'https://t.me/Cosmverse_community'} width='19' height='19'>
+                  <FaTelegram />
+                </SocialButton>
+                <SocialButton label={'Discord'} href={'https://discord.gg/6tbnz7ss'} width='19' height='19'>
+                  <FaDiscord />
+                </SocialButton>
 
-            </Stack>
+              </Stack>
 
-
-          </Flex>
-
-
-        </GridItem>
-        </Grid>
+      </Box>
+    </React.Fragment>
 
 
-
-
-
-    </Flex>
 
   );
 }
